1adadea14243396294acd47660981b8968d5d79f
chrisdevwords <chrisdevwords@yahoo.com>
2015-05-09 09:02:07 -0400

    adding a spec file (empty for now) for VigodaBot and the Wikipedia parser

---
+++ tests/app/lib/Wikipedia_spec.js
@@ -0,0 +1,9 @@
+var should = require('should'),
+    sinon = require('sinon'),
+    request = require('request'),
+    Wikipedia = require('../../../app/lib/Wikipedia'),
+    mock = require('./../../mock/index');
+
+describe('Wkipedia API helper', function () {
+
+});
